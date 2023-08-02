e2a6ba2ee3fa4067520598d83c0f2f975eded08f
Adel5002 <aromat29.11@gmail.com>
2023-08-02 12:08:49 +0300

    infinite pagination realisation

--- Vitamins/views.py
+++ Vitamins/views.py
@@ -3,42 +3,46 @@ from django.urls import reverse
 from django.views.decorators.http import require_POST
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
+from django.http import QueryDict
+
 
 from .cart import Cart
 from .models import Product, Comment, Category
 from .forms import CommentForm, CartAddProductForm, CartSubtractProductForm
 
 
+
+
 class ProdictList(ListView):
     model = Product
     template_name = 'index.html'
     context_object_name = 'products'
-
-    # def get_queryset(self):
-    #     self.filter = ProductFilter(self.request.GET, queryset=Product.objects.all())
-    #
-    #     return self.filter.qs
+    paginate_by = 8
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         cart_product_form = CartAddProductForm()
-
-        # context['filter'] = self.filter
         context['cart_product_form'] = cart_product_form
         return context
-
-
 
 
 class ProductDetails(DetailView):
     model = Product
     template_name = 'product_details.html'
     context_object_name = 'product'
+    comments_paginate_by = 1
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         avg_rating = Comment.objects.filter(product=self.object).aggregate(rating=Avg('rating'))
+        cart_product_form = CartAddProductForm()
+        comments = Comment.objects.filter(product=self.object)
+        paginator = Paginator(comments, self.comments_paginate_by)
+        page_number = self.request.GET.get('page')
 
+        context['comments'] = paginator.get_page(page_number)
+        context['cart_product_form'] = cart_product_form
         context['avg_rating'] = avg_rating
         return context
 
@@ -47,6 +51,7 @@ class CategoryListView(ListView):
     model = Product
     template_name = 'products_by_categories.html'
     context_object_name = 'products'
+    paginate_by = 8
 
     def get_queryset(self):
         slug = self.kwargs['slug']
@@ -65,16 +70,22 @@ class SearchProducts(ListView):
     model = Product
     template_name = 'search_product.html'
     context_object_name = 'products'
-
+    paginate_by = 2
     def get_queryset(self):
-        query = self.request.GET.get('q')
-        object_list = Product.objects.filter(title__iregex=query)
-        return object_list
+        queryset = super().get_queryset()
+        query = self.request.GET.get('title')
+        if query:
+            queryset = queryset.filter(title__iregex=query)
+        return queryset
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         cart_product_form = CartAddProductForm()
+        search_params = self.request.GET.copy()
+        if 'page' in search_params:
+            del search_params['page']
 
+        context['search_params'] = search_params.urlencode()
         context['cart_product_form'] = cart_product_form
         return context
 
