6e2d5ee99829cde2033c920512146c99e1949186
Adel5002 <aromat29.11@gmail.com>
2023-09-05 19:46:28 +0300

    Little update, not worth the attention

--- Vitamins/models.py
+++ Vitamins/models.py
@@ -23,8 +23,8 @@ RATING = (
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=120)
-    slug = models.SlugField(unique=True, db_index=True)
+    name = models.CharField('Название', max_length=120)
+    slug = models.SlugField('Слаг', unique=True, db_index=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(unidecode(self.name))
@@ -40,7 +40,7 @@ class Category(models.Model):
 
 
 class Vendor(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField('Название', max_length=120)
 
     def __str__(self):
         return self.name
@@ -51,30 +51,30 @@ class Vendor(models.Model):
 
 
 class Product(models.Model):
-    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+    categories = models.ForeignKey( Category, on_delete=models.CASCADE)
     vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, help_text='Указывать не обяз \
                                                                                  ательно, это просто задел на будущее!')
 
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    old_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, help_text='Указ\
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    old_price = models.DecimalField('Старая цена', max_digits=10, decimal_places=2, null=True, blank=True, help_text='Указ\
                                             ывать в том случае если хотите сделать скидку')
 
-    image = models.ImageField(upload_to='vitamins/')
-    title = models.CharField(max_length=120)
-    description = models.TextField(null=True, blank=True)
-    dateCreation = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(null=True, blank=True)
-    slug = models.SlugField(unique=True, db_index=True)
-    specifications = models.TextField()
-    release_form = models.CharField(max_length=120, null=True)
-    age = models.PositiveSmallIntegerField(null=True)
-    country = models.CharField(max_length=120, null=True)
-    is_available = models.BooleanField(default=True)
+    image = models.ImageField('Изображение', upload_to='vitamins/')
+    title = models.CharField('Название продукта', max_length=120)
+    description = models.TextField('Описание', null=True, blank=True)
+    dateCreation = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated = models.DateTimeField('Дата обновления', null=True, blank=True)
+    slug = models.SlugField('Слаг', unique=True, db_index=True)
+    specifications = models.TextField('Состав')
+    release_form = models.CharField('Форма выпуска', max_length=120, null=True)
+    age = models.PositiveSmallIntegerField('Возраст', null=True)
+    country = models.CharField('Страна производитель', max_length=120, null=True)
+    is_available = models.BooleanField('В наличии', default=True)
     status = models.BooleanField(default=True)
     featured = models.BooleanField(default=False)
     digital = models.BooleanField(default=False)
-    quantity = models.PositiveIntegerField(default=1, null=True, blank=True)
+    quantity = models.PositiveIntegerField('Количество', default=1, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(unidecode(self.title))
@@ -129,17 +129,17 @@ class Comment(models.Model):
 
 class CartOrder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
+    first_name = models.CharField('Имя', max_length=50)
+    last_name = models.CharField('Фамилия', max_length=50)
+    email = models.EmailField('Email')
+    address = models.CharField('Адрес', max_length=250)
+    postal_code = models.CharField('Почтовый индекс', max_length=20)
+    city = models.CharField('Город', max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=False)
-    phone_number = models.BigIntegerField(null=True)
-    track_number = models.CharField(max_length=120, null=True, blank=True)
+    paid = models.BooleanField('Статус платежа', default=False)
+    phone_number = models.BigIntegerField('Номер телефона', null=True)
+    track_number = models.CharField('Трекномер', max_length=120, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Заказ'
@@ -163,8 +163,8 @@ class CartOrder(models.Model):
 class CartOrderItem(models.Model):
     order = models.ForeignKey(CartOrder, on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    qty = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    qty = models.PositiveIntegerField('Количество', default=1)
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def get_cost(self):
